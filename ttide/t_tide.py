b8dcb85668f988d5921fc3edd389a0f11e497a33
Oleksandr Huziy <oleksandr.huziy@canada.ca>
2021-08-26 19:16:52 +0000

    Specify named argument axis=1
    in the call to np.median, for numba compilation

--- ttide/t_tide.py
+++ ttide/t_tide.py
@@ -591,8 +591,8 @@ def t_tide(xin, dt=1, stime=None, lat=None,
             errval = np.multiply(
                 np.median(
                     np.absolute(
-                        para - (np.median(para, 1).reshape(-1, 1) *
-                                np.ones([1, nreal]))), 1) / 0.6375, 1.96)
+                        para - (np.median(para, axis=1).reshape(-1, 1) *
+                                np.ones([1, nreal]))), axis=1) / 0.6375, 1.96)
             return errval
 
         emaj = booterrcalc(fmaj, nreal)
